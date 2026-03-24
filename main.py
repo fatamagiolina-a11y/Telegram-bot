b8f4afa4790adb6667b809d203d7eebc0893e085
fatamagiolina-a11y <fatamagiolina@gmail.com>
2026-03-24 10:43:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,6 @@ async def handle_channel_post(update: Update, context: ContextTypes.DEFAULT_TYPE
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.ALL, handle_channel_post))
+app.add_handler(MessageHandler(filters.ChatType.CHANNEL, handle_channel_post))
 
 app.run_polling()
