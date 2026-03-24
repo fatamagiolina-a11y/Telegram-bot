d806e41e7ae68ec9e18daeeacb97c4d823905ec4
fatamagiolina-a11y <fatamagiolina@gmail.com>
2026-03-24 09:59:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-import os
 from telegram import Update
-from telegram.ext import ChannelPostHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, ChannelPostHandler
 
 TOKEN = "8659770527:AAEvO0PsPMPOwDvZA0G6d5TX2XdqmdK8cDU"
-
 TARGET_CHAT_ID = 2028499794
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Это бот PIKANTO 🔥")
+    await update.message.reply_text("PIKANTO bot работает 🔥")
 
 async def handle_channel_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.channel_post
 
-    caption = message.caption if message.caption else message.text if message.text else ""
-    new_caption = caption + "\n\n📲 Заказать: https://wa.me/393516282355
+    text = ""
+    if message.caption:
+        text = message.caption
+    elif message.text:
+        text = message.text
+
+    text += "\n\n📲 Заказать: https://wa.me/393516282355"
 
     if message.photo:
         await context.bot.send_photo(
             chat_id=TARGET_CHAT_ID,
             photo=message.photo[-1].file_id,
-            caption=new_caption
+            caption=text
         )
     else:
         await context.bot.send_message(
             chat_id=TARGET_CHAT_ID,
-            text=new_caption
+            text=text
         )
 
 app = ApplicationBuilder().token(TOKEN).build()
@@ -33,3 +36,7 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(ChannelPostHandler(handle_channel_post))
 
 app.run_polling()
+
+
+
+
